c2f184ce4c4ba6b0473a3f15c50b273378387b94
Cedric Schwyter <cedricschwyter@bluewin.ch>
2021-10-16 14:18:59 +0200

    Add UEFI support

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,15 @@
-#![no_std]
 #![no_main]
+#![no_std]
+#![feature(abi_efiapi)]
 
-use core::panic::PanicInfo;
+extern crate rlibc;
 
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
+use uefi::prelude::*;
+use uefi::ResultExt;
 
-static HELLO: &[u8] = b"Hello World!";
+#[entry]
+fn efi_main(_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
+    uefi_services::init(&mut system_table).unwrap_success();
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-    loop {}
+    Status::SUCCESS
 }
