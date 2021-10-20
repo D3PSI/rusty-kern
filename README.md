d2995eb8dad90530144bdee796d6bc26222a40e2
Cedric Schwyter <cedricschwyter@bluewin.ch>
2021-10-20 19:57:56 +0200

    Me being a big dummy just now realized that the screen buffer here is a buffer of individual pixels lol, needs work

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # rusty-kern
+## building
+clone the repository:
+
+    git clone https://github.com/D3PSI/rusty-kern.git
+
+### dependencies
+a nightly rust toolchain is required. installation using rustup:
+
+    rustup install nightly
+    rustup override set nightly
+
+## building a bootable image
+after installing all dependencies you should be able to build a bootable image as follows:
+
+    cargo kimage
--- src/print.rs
+++ src/print.rs
@@ -4,42 +4,69 @@ use bootloader::boot_info::FrameBuffer;
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u8)]
 pub enum Color {
-    Black = 0,
-    Blue = 1,
-    Green = 2,
-    Cyan = 3,
-    Red = 4,
-    Magenta = 5,
-    Brown = 6,
-    LightGray = 7,
-    DarkGray = 8,
-    LightBlue = 9,
-    LightGreen = 10,
-    LightCyan = 11,
-    LightRed = 12,
-    Pink = 13,
-    Yellow = 14,
-    White = 15,
+    Black = 0x0,
+    Blue = 0x1,
+    Green = 0x2,
+    Cyan = 0x3,
+    Red = 0x4,
+    Magenta = 0x5,
+    Brown = 0x6,
+    LightGray = 0x7,
+    DarkGray = 0x8,
+    LightBlue = 0x9,
+    LightGreen = 0xA,
+    LightCyan = 0xB,
+    LightRed = 0xC,
+    Pink = 0xD,
+    Yellow = 0xE,
+    White = 0xF,
 }
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(transparent)]
-struct ColorCode(u8);
+pub struct ColorCode(u8);
 
 impl ColorCode {
-    fn new(foreground: Color, background: Color) -> ColorCode {
+    pub fn new(foreground: Color, background: Color) -> ColorCode {
         ColorCode((background as u8) << 4 | (foreground as u8))
     }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(C)]
-struct ScreenChar {
-    ascii_character: u8,
-    color_code: ColorCode,
+pub struct Writer {
+    pub column_position: usize,
+    pub buffer: &'static mut FrameBuffer,
+    pub color_code: ColorCode,
 }
 
-pub struct Writer {
-    column_position: usize,
-    color_code: ColorCode,
-    buffer: &'static mut [u8],
+impl Writer {
+    pub fn write_byte(&mut self, byte: u8) {
+        match byte {
+            b'\n' => self.new_line(),
+            byte => {
+                if self.column_position >= self.buffer.info().horizontal_resolution {
+                    self.new_line();
+                }
+
+                let row = self.buffer.info().vertical_resolution - 1;
+                let col = self.column_position;
+                let vertical_resolution = self.buffer.info().vertical_resolution;
+
+                for byte in self.buffer.buffer_mut() {
+                    *byte = 0x90;
+                }
+                self.column_position += 1;
+            }
+        }
+    }
+
+    fn new_line(&mut self) {}
+
+    pub fn write_string(&mut self, s: &str) {
+        for byte in s.bytes() {
+            match byte {
+                0x20..=0x7e | b'\n' => self.write_byte(byte),
+                _ => self.write_byte(0xfe),
+            }
+        }
+    }
 }
